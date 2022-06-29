d38318094e300736629789586127d22cb62421b1
Fabio <fabiojuniorm@gmail.com>
2022-06-29 16:48:40 -0300

    Criação de calculadora

--- src/com/dio/PrimeiroPrograma.java
+++ src/com/dio/PrimeiroPrograma.java
@@ -6,9 +6,9 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Gato gato = new Gato();
+		//Gato gato = new Gato();
 		
-		System.out.println(gato);
+		//System.out.println(gato);
 		/*int a = 2;
 		int b = 3;
 		System.out.println("Hello World! " + (a+b));*/
