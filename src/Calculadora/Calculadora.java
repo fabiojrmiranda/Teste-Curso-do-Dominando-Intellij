6006c9dfbf75c5c19d38feaca47a612a6d054464
Fabio <fabiojuniorm@gmail.com>
2022-06-29 16:49:55 -0300

    Criação da calculadora

---
+++ src/Calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package Calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		System.out.println("Digite o primeiro valor ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor ");
+		b = scan.nextInt();
+		
+		int soma = soma(a, b);
+		int subtracao = subtracao(a, b);
+		int multiplicacao = multiplicacao(a, b);
+		int divisao = divisao = divisao(a, b);
+		
+		System.out.println("Soma " + soma);
+		System.out.println("Subtracao " + subtracao);
+		System.out.println("Multiplicacao " + multiplicacao);
+		System.out.println("Divisao " + divisao);		
+	}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+	public static int divisao(int a, int b) {
+		return a / b;
+	}
+	
+	
+
+}
