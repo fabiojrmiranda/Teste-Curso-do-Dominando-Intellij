82a03f21d2bc23fe0a3bfb42fcb554fc1cd4fd07
Fabio <fabiojuniorm@gmail.com>
2022-07-06 16:33:45 -0300

    Primeiro Commit

---
+++ Primeiro programa Intellij/src/br/com/dio/PrimeiroProgramaJava.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a + b));*/
+        Gato gato = new Gato();
+        System.out.println(gato);
+    }
+}
