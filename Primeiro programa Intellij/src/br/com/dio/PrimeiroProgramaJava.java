a0f5e0e5e054cb0f7bd8301176dea43331b7445c
Fabio <fabiojuniorm@gmail.com>
2022-07-06 16:51:47 -0300

    Criando classe livro

--- Primeiro programa Intellij/src/br/com/dio/PrimeiroProgramaJava.java
+++ Primeiro programa Intellij/src/br/com/dio/PrimeiroProgramaJava.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroProgramaJava {
     public static void main(String[] args) {
         /*int a = 5;
@@ -9,5 +11,54 @@ public class PrimeiroProgramaJava {
         System.out.println("Hello World! " + (a + b));*/
         Gato gato = new Gato();
         System.out.println(gato);
+        Livro livro = new Livro("O problema dos 3 corpos",300);
+        System.out.println(livro);
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
